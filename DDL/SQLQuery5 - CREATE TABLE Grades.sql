483de88907be303e496a971ed047c8eaad0a3de2
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2025-02-05 22:55:40 +0300

    DBMS DONE

--- DDL/SQLQuery5 - CREATE TABLE Grades.sql
+++ DDL/SQLQuery5 - CREATE TABLE Grades.sql
@@ -9,6 +9,6 @@ CREATE TABLE Grades
 	grade_1		TINYINT		NULL,
 	grade_2		TINYINT		NULL,
 	PRIMARY KEY(student,lesson),
-	CONSTRAINT FK_G_student_2_Students		FOREIGN KEY(student)		REFERENCES Students(student_id),
-	CONSTRAINT FK_G_lesson_2_Schedule		FOREIGN KEY(lesson)			REFERENCES Schedule(lesson_id)
+	CONSTRAINT FK_G_student_2_Students	FOREIGN KEY(student)	REFERENCES Students(student_id),
+	CONSTRAINT FK_G_lesson_2_Schedule	FOREIGN KEY(lesson)		REFERENCES Schedule(lesson_id)
 );
