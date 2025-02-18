30d43387083fafdb773fbaf218978e84dd8b9a2d
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2025-02-18 21:27:59 +0300

    Schedule of period / Salary DONE

--- T-SQL/SQLQuery2 - Schedule procedures test.sql
+++ T-SQL/SQLQuery2 - Schedule procedures test.sql
@@ -4,4 +4,5 @@ GO
 EXEC sp_SetScheduleForStacionarGroup N'PV_319', N'%UML%', N'Ковтун', N'2024-08-10', N'18:30';
 
 EXEC sp_PrintScheduleForGroup N'PV_319';
+EXEC sp_PrintScheduleForGroupPeriod N'PV_319', '2024-06-01', '2024-09-01';
 --SELECT * FROM Schedule;
